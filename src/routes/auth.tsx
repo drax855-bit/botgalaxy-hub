60aa897c41589f4c705a44d3dde3ab90b1f79ae1
Drax855 <draxgaming855@gmail.com>
2026-07-30 23:42:33 +0530

    Update auth.tsx

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,1 +1,13 @@
+import { createFileRoute } from '@tanstack/react-router'
 
+export const Route = createFileRoute('/auth')({
+  component: AuthPage,
+})
+
+function AuthPage() {
+  return (
+    <div>
+      Auth Page
+    </div>
+  )
+}
